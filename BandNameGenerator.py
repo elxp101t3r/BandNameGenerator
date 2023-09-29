2cc30aa8db67a1c7e1f1d1688c1ac1d571f1dfe8
elxp101t3r <eliotx10101@outlook.com>
2023-09-29 20:40:19 +0300

    It is day1 of a challenge that starts from nothing to hero code

---
+++ BandNameGenerator.py
@@ -0,0 +1,7 @@
+def main():
+    print('Welcome to the Band Name Generator.\n')
+    color = input('What is your favourite color?\n')
+    genre = input('What kind of music you hear most of the time?\n')
+    print('Your band name is '+color+genre)
+if __name__ == "__main__":
+    main()
